15aac166c8abfce24f8b06875c73dd2fce47c4b0
cinthiabs <cinthiabarbosa8d@outlook.com>
2025-01-29 20:29:02 -0300

    create model and controller to Indentity

--- NerdStoreEnterprise/src/services/NSE.Identidade.API/Program.cs
+++ NerdStoreEnterprise/src/services/NSE.Identidade.API/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using NSE.Identidade.API.Models;
+using NSE.Identidade.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
